7211bad4c1f8d30870ef241599a7a3f2967acb50
Lakshmi Muralidharan <lamdaw@gmail.com>
2020-04-10 11:49:41 -0500

    updating with some more codes

--- reference_scripts/eda_helper.py
+++ reference_scripts/eda_helper.py
@@ -3,11 +3,24 @@
 Give me six hours to chop down a tree and I will spend the first four sharpening the axe. - Abraham Lincoln
 
 """
+import numpy as np
 import pandas as pd
 import pandas_profiling
+from sklearn.datasets import fetch_openml
 
-data = pd.read_csv('/Users/lakshmi/projects/nasa_engine_degradation/CMAPSSData/train_FD001.txt')
-profile = pandas_profiling.ProfileReport(data, title='Pandas Profiling Report', html={'style':{'full_width':True}})
-profile.to_file(output_file="your_report.html")
+
+# open data sets : https://www.openml.org/search?type=data 
+
+cardiotocography = fetch_openml(name='diabetes', version=1)
+
+cardio_df = pd.DataFrame(data= np.c_[cardiotocography['data'], cardiotocography['target']], columns= cardiotocography['feature_names'] + ['target'])
+
+print(cardio_df.head())
+
+######## Pandas profiling for easy EDA and pair plotting
+
+profile = pandas_profiling.ProfileReport(cardio_df, title='Pandas Profiling Report', html={'style':{'full_width':True}})
+
+profile.to_file(output_file="cardio_report.html")
 
 
